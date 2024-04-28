bc7c0c9e7234eb82c3688e74e1a5cae392252227
izekorphilip <izekorphilip5@gmail.com>
2024-04-28 21:36:29 +0100

    Another commit from week 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,24 +5,47 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Philip World!");
-        
-         DisplayWelcome ();
 
-         string myUserName = PromptUserName();
+        DisplayWelcome();
 
-    } 
-        static void DisplayWelcome()
-        {
-            Console.WriteLine("Welcome to the Program!");
-        }
+        string myUserName = PromptUserName();
 
-        static string PromptUserName()
-        { 
-            Console.Write("What is your userName? ");
-            string userName = Console.ReadLine();
-            return userName;
-        }
+        int number = PromptUserNumber();
+
+        int squaredNumb = SquareNumber(number);
         
-        
-        
+
+        DisplayResult(myUserName, squaredNumb);
+
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your userName? ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favourite number? ");
+        int favouriteNumber = int.Parse(Console.ReadLine());
+        return favouriteNumber;
+    }
+
+    static int SquareNumber(int squaredNum)
+    {
+        int squaredNumber = squaredNum * squaredNum;
+        return squaredNumber;
+    }
+
+    static void  DisplayResult(string name, int squared)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squared}");
+    }
+
 }
